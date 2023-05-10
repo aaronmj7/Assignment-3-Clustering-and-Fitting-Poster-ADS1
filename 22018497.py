a1a329f94dd298a9bbf791fe29cfb938872174a4
Aaron M Joseph <125889154+aaronmj7@users.noreply.github.com>
2023-05-10 19:52:18 +0100

    Add files via upload

---
+++ 22018497.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  9 22:14:14 2023
+
+@author: aaron
+"""
+
+import pandas as pd
+from sklearn.cluster import KMeans
+import cluster_tools as ct
+import matplotlib.pyplot as plt
+
