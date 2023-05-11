8510bfe17f3131c1ae17f23f64bdedeeecd37353
Aaron M Joseph <125889154+aaronmj7@users.noreply.github.com>
2023-05-11 16:55:18 +0100

    Add files via upload

--- 22018497.py
+++ 22018497.py
@@ -5,11 +5,14 @@ Created on Tue May  9 22:14:14 2023
 @author: aaron
 """
 
+import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
 import cluster_tools as ct
 import matplotlib.pyplot as plt
 import seaborn as sns
+import scipy.optimize as opt
+import errors as err
 
 
 def read_df(fname):
@@ -25,8 +28,11 @@ def read_df(fname):
     # dropping columns that are not needed
     df.drop(["Country Code", "Indicator Code", "Indicator Name"], axis=1,
             inplace=True)
+    df
     # setting index
     df.set_index(['Country Name'], inplace=True)
+    # naming column
+    df.columns.name = "Year"
     # droping columns and rows with nan values
     df.dropna(how="all", axis=1, inplace=True)
     df.dropna(how="any", thresh=50, inplace=True)
@@ -35,37 +41,43 @@ def read_df(fname):
 
     # transposing
     df_t = df.transpose()
+    # change dtype to int
+    df_t.index = pd.to_numeric(df_t.index)
     # droping columns with nan values
     df_t.dropna(how='all', axis=1, inplace=True)
-
     return df, df_t
 
 
-def cluster_plot(nc):
-    ''' Function to plot clustering graph with
+def cluster_plot(df, nc):
+    ''' Function to plot clustering graph.
+    Arguments:
+        A dataframe.
+        The number of clusters needed.
+    Returns the cluster centres and array of labels.
+
     '''
     # set up the clusterer with the number of expected clusters
     kmeans = KMeans(n_clusters=nc)
     # fit data
-    kmeans.fit(df_normalised)
+    kmeans.fit(df)
     # extracting labels
     labels = kmeans.labels_
     # adding labels to df
-    df_normalised['Labels'] = labels
+    df['Labels'] = labels
     # extracting cluster centres
     cen = kmeans.cluster_centers_
 
     # plotting
-    plt.figure(figsize=(6, 6))
+    plt.figure(figsize=(7, 7))
 
     # scatter plot
     colors = ['red', 'blue', 'green', 'orange', 'pink']
-    labels = ['Cluster '+str(i+1) for i in range(nc)]
+    labeling = ['Cluster '+str(i+1) for i in range(nc)]
 
     for i in range(nc):
-        plt.scatter(df_normalised[df_normalised['Labels'] == i]['1965'],
-                    df_normalised[df_normalised['Labels'] == i]['2015'],
-                    c=colors[i], label=labels[i])
+        plt.scatter(df[df['Labels'] == i]['1965'],
+                    df[df['Labels'] == i]['2015'],
+                    c=colors[i], label=labeling[i])
 
     # plot cluster centres
     xc = cen[:, 0]
@@ -77,13 +89,36 @@ def cluster_plot(nc):
     plt.title(str(nc) + " Clusters")
     plt.legend()
     plt.show()
-    return
+    return cen, labels
 
 
+def logistic(x, a, b, c):
+    f = c / (1 + np.exp(-a*(x-b)))
+    return f
+
+
+def poly(t, c0, c1, c2, c3):
+    """ Computes a polynominal c0 + c1*t + c2*t^2 + c3*t^3
+    Arguments:
+        x value
+        Constant
+        Coefficient of x
+        Coefficient of x^2
+        Coefficient of x^3
+    Returns f(x)
+    """
+    f = c0 + c1*t + c2*t**2 + c3*t**3
+    return f
+
+
+# read the data
 df_ogc, df_ogc_t = read_df("API_EG.ELC.FOSL.ZS_DS2_en_csv_v2_5211704.csv")
 
 # setting seaborn theme
 sns.set_theme()
+
+'''
+# clustering
 
 # plotting scatter matrix to find good cluster
 years = ['1960', '1961', '1962', '1963', '1964', '1965',
@@ -116,7 +151,60 @@ plt.ylabel('WCSS')
 plt.show()
 
 # plot for three clusters
-cluster_plot(3)
+cen3, labels3 = cluster_plot(df_normalised, 3)
 
 # plot for four clusters
-cluster_plot(4)
+cen4, labels4 = cluster_plot(df_normalised, 4)
+
+# backscale the normalised df
+cen_bckscl = ct.backscale(cen4, df_min, df_max)
+
+# backscaled cluster graph
+plt.figure(figsize=(7, 7))
+
+plt.scatter(df_ogc["1990"], df_ogc["2015"], c=labels4, cmap='Set1')
+
+xcen = cen_bckscl[:, 0]
+ycen = cen_bckscl[:, 1]
+
+plt.scatter(xcen, ycen, 45, "k", marker="d")
+
+plt.xlabel("1965")
+plt.ylabel("2015")
+plt.title("Backscaled Cluster Graph")
+plt.show()
+'''
+
+# fitting
+
+# data to fit
+df_tofit = pd.DataFrame(df_ogc_t.iloc[:, 3].copy())
+
+# plot data to be fitted
+df_tofit.plot(title='Electricity Produced from Oil, Gas and Coal Sources',
+              ylabel='Electricity Produced(% of Total)')
+plt.show()
+
+# fitting polynomial function
+param, covar = opt.curve_fit(logistic, df_tofit.index, df_tofit.iloc[:, 0],
+                             p0=(0.0, 1987.5, 100))
+
+# add fit values to dataframe
+df_tofit["fit"] = logistic(df_tofit.index, *param)
+
+plt.figure()
+
+plt.plot(df_tofit.index, df_tofit.iloc[:, 0], label="data")
+plt.plot(df_tofit.index, df_tofit["fit"], label="fit")
+
+# calculate sigma
+sigmas = np.sqrt(np.diag(covar))
+
+# calculate upper and lower limits
+years = np.arange(1960, 2016)
+lower, upper = err.err_ranges(years, logistic, param, sigmas)
+# plot error ranges
+plt.fill_between(years, lower, upper, alpha=0.5)
+
+plt.legend()
+plt.show()
