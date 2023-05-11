b3e41fe28ce140149f2aa00287c030945dc0ec6c
Aaron M Joseph <125889154+aaronmj7@users.noreply.github.com>
2023-05-11 17:59:26 +0100

    Add files via upload

--- 22018497.py
+++ 22018497.py
@@ -48,6 +48,31 @@ def read_df(fname):
     return df, df_t
 
 
+def elbow_method(df, n):
+    ''' Function to plot Elbow method graph.
+    Argument:
+        A dataframe
+        Maximum number of clusters to check.
+    '''
+    # calculating wcss for different no. of clusters
+    wcss = []
+    for i in range(1, n+1):
+        # set up kmeans and fit
+        kmeans = KMeans(n_clusters=i, max_iter=300, n_init=10)
+        kmeans.fit(df)
+        wcss.append(kmeans.inertia_)
+
+    # plotting to use elbow method
+    plt.figure(figsize=(14, 7))
+
+    plt.plot(range(1, n+1), wcss, marker='o')
+
+    plt.title('The Elbow method')
+    plt.xlabel('Number of Clusters')
+    plt.ylabel('WCSS')
+    plt.show()
+
+
 def cluster_plot(df, nc):
     ''' Function to plot clustering graph.
     Arguments:
@@ -92,22 +117,10 @@ def cluster_plot(df, nc):
     return cen, labels
 
 
-def logistic(x, a, b, c):
-    f = c / (1 + np.exp(-a*(x-b)))
-    return f
-
-
-def poly(t, c0, c1, c2, c3):
-    """ Computes a polynominal c0 + c1*t + c2*t^2 + c3*t^3
-    Arguments:
-        x value
-        Constant
-        Coefficient of x
-        Coefficient of x^2
-        Coefficient of x^3
-    Returns f(x)
-    """
-    f = c0 + c1*t + c2*t**2 + c3*t**3
+def logistic(t, g, t0, n0):
+    '''Calculates the logistic function with scale factor n0 and growth rate g
+    '''
+    f = n0 / (1 + np.exp(-g*(t-t0)))
     return f
 
 
@@ -115,15 +128,17 @@ def poly(t, c0, c1, c2, c3):
 df_ogc, df_ogc_t = read_df("API_EG.ELC.FOSL.ZS_DS2_en_csv_v2_5211704.csv")
 
 # setting seaborn theme
-sns.set_theme()
+sns.set_theme(font_scale=1.25)
 
-'''
+
 # clustering
 
 # plotting scatter matrix to find good cluster
 years = ['1960', '1961', '1962', '1963', '1964', '1965',
          '2010', '2011', '2012', '2013', '2014', '2015']
-pd.plotting.scatter_matrix(df_ogc[years], figsize=(12, 12))
+
+pd.plotting.scatter_matrix(df_ogc[years], figsize=(12, 12), )
+
 plt.show()
 
 # chosing best years for clustering
@@ -132,23 +147,8 @@ df_clstr = df_ogc[['1965', '2015']].copy()
 # normalising and storing minimum and maximum
 df_normalised, df_min, df_max = ct.scaler(df_clstr)
 
-# calculating wcss for different no. of clusters
-wcss = []
-for i in range(1, 10):
-    # set up kmeans and fit
-    kmeans = KMeans(n_clusters=i, max_iter=300, n_init=10)
-    kmeans.fit(df_normalised)
-    wcss.append(kmeans.inertia_)
-
-# plotting to use elbow method
-plt.figure(figsize=(14, 7))
-
-plt.plot(range(1, 10), wcss, marker='o')
-
-plt.title('The Elbow method')
-plt.xlabel('Number of clusters')
-plt.ylabel('WCSS')
-plt.show()
+# using elbow method for finding best number of cluster
+elbow_method(df_normalised, 10)
 
 # plot for three clusters
 cen3, labels3 = cluster_plot(df_normalised, 3)
@@ -173,7 +173,7 @@ plt.xlabel("1965")
 plt.ylabel("2015")
 plt.title("Backscaled Cluster Graph")
 plt.show()
-'''
+
 
 # fitting
 
@@ -183,6 +183,7 @@ df_tofit = pd.DataFrame(df_ogc_t.iloc[:, 3].copy())
 # plot data to be fitted
 df_tofit.plot(title='Electricity Produced from Oil, Gas and Coal Sources',
               ylabel='Electricity Produced(% of Total)')
+plt.xlim(1960, 2015)
 plt.show()
 
 # fitting polynomial function
@@ -192,19 +193,27 @@ param, covar = opt.curve_fit(logistic, df_tofit.index, df_tofit.iloc[:, 0],
 # add fit values to dataframe
 df_tofit["fit"] = logistic(df_tofit.index, *param)
 
-plt.figure()
+plt.figure(figsize=(10, 5))
 
-plt.plot(df_tofit.index, df_tofit.iloc[:, 0], label="data")
-plt.plot(df_tofit.index, df_tofit["fit"], label="fit")
+plt.plot(df_tofit.index, df_tofit.iloc[:, 0], c='dodgerblue', label="data")
+plt.plot(df_tofit.index, df_tofit["fit"], c='k', label="fit")
 
 # calculate sigma
 sigmas = np.sqrt(np.diag(covar))
 
+# extend years for prediction
+years = np.arange(1960, 2041)
 # calculate upper and lower limits
-years = np.arange(1960, 2016)
 lower, upper = err.err_ranges(years, logistic, param, sigmas)
 # plot error ranges
-plt.fill_between(years, lower, upper, alpha=0.5)
+plt.fill_between(years, lower, upper, color='orange', alpha=0.5)
+
+plt.xticks(range(1960, 2041, 10))
+plt.xlim(1960, 2040)
+plt.xlabel('Years')
+plt.ylabel('Electricity Produced(% of Total)')
+title = "Fitting Canada's Electricity Produced from Oil, Gas and Coal Sources"
+plt.title(title)
 
 plt.legend()
 plt.show()
